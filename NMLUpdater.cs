20f3d9aa7be9a04160fd9eb756b477a3058ffadf
AlexW-578 <alex.baskerville@outlook.com>
2023-04-29 16:38:41 +0100

    Removed dependency for external program

--- NMLUpdater.cs
+++ NMLUpdater.cs
@@ -1,22 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
-using System.Threading;
-using FrooxEngine;
-using FrooxEngine.LogiX;
-using HarmonyLib;
 using NeosModLoader;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using BaseX;
-using FrooxEngine.LogiX.WorldModel;
-using FrooxEngine.UIX;
+
 
 namespace NMLUpdater
 {
@@ -24,19 +10,15 @@ namespace NMLUpdater
     {
         public override string Name => "NMLUpdater";
         public override string Author => "AlexW-578";
-        public override string Version => "1.0.0";
+        public override string Version => "1.0.1";
         public override string Link => "https://github.com/AlexW-578/NMLUpdater";
 
-        private static List<NeosModUpdate> ModsThatNeedUpdating = new List<NeosModUpdate>();
-
-        public class NeosModUpdate
+        private class NeosModUpdate
         {
             public string Name { get; set; }
             public string Url { get; set; }
-            public bool NeedsUpdate { get; set; }
             public string NewVersion { get; set; }
             public string OldVersion { get; set; }
-
             public string Sha256 { get; set; }
         }
 
@@ -46,19 +28,7 @@ namespace NMLUpdater
         [AutoRegisterConfigKey] private static readonly ModConfigurationKey<bool> Enabled =
             new ModConfigurationKey<bool>("Enabled", "Enable/Disable the Mod", () => true);
 
-        [AutoRegisterConfigKey] private static readonly ModConfigurationKey<bool> ListToFile =
-            new ModConfigurationKey<bool>("ModListToFile",
-                "Create a json file with all the mods that needs updating.",
-                () => false);
 
-        [AutoRegisterConfigKey] private static readonly ModConfigurationKey<bool> SpawnProgram =
-            new ModConfigurationKey<bool>("SpawnProgram",
-                "Launches a External Program To update mods.\nMust have ModListToFile Enabled.",
-                () => false);
-
-        private static string SpawnProgramDir = "\\nml_updater\\updater.exe";
-        private static bool _isSpawned = false;
-        
         public override void OnEngineInit()
         {
             Config = GetConfiguration();
@@ -68,113 +38,64 @@ namespace NMLUpdater
                 return;
             }
 
-            Harmony harmony = new Harmony("co.uk.alexw-578.NMLUpdater");
             const string manifestUrl =
                 "https://raw.githubusercontent.com/neos-modding-group/neos-mod-manifest/master/manifest.json";
             string manifest = GetManifest((manifestUrl));
-            JObject manifestJson = JObject.Parse(manifest);
-            GetModList("./nml_mods", manifestJson);
-            if (Config.GetValue(ListToFile))
-            {
-                if (Directory.Exists("./nm_updater") == false)
-                {
-                    Directory.CreateDirectory("./nml_updater");
-                }
-
-                string text = "";
-                foreach (NeosModUpdate mod in ModsThatNeedUpdating)
-                {
-                    text += $"{JsonConvert.SerializeObject(mod)}\n";
-                }
-
-
-                File.WriteAllText("./nml_updater/mods.json", text);
-            }
-
-            if (Config.GetValue(SpawnProgram))
-            {
-                Platform();
-                Engine.Current.OnShutdownRequest += _ => { StartExternalProgram(); };
-            }
-
-            harmony.PatchAll();
+            GetVersionsFromManifest(JObject.Parse(manifest));
         }
 
-        private void Platform()
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                SpawnProgramDir = "/nml_updater/updater.sh";
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                SpawnProgramDir = "\\nml_updater\\updater.exe";
-            }
-        }
 
-        private static void GetModList(string modDir, JObject manifestJson)
+        private static void GetVersionsFromManifest(JObject manifestJson)
         {
-            foreach (NeosModBase mod in ModLoader.Mods())
+            foreach (NeosModBase neosMod in ModLoader.Mods())
             {
-                NeosModUpdate neosModUpdate = GetVersionsFromManifest(manifestJson, mod);
-                if (neosModUpdate.NeedsUpdate)
+                NeosModUpdate modUpdate = new NeosModUpdate();
+                JToken modList = manifestJson["mods"];
+                modUpdate.Name = neosMod.Name;
+                modUpdate.OldVersion = neosMod.Version;
+                foreach (JToken mod in modList.Children())
                 {
-                    ModsThatNeedUpdating.Add(neosModUpdate);
-                    Warn($"{neosModUpdate.Name}: New Version:{neosModUpdate.NewVersion} - Old Version: {neosModUpdate.OldVersion}");
-                }
-            }
-        }
-
-        private static NeosModUpdate GetVersionsFromManifest(JObject manifestJson, NeosModBase neosMod)
-        {
-            NeosModUpdate modUpdate = new NeosModUpdate();
-            JToken modList = manifestJson["mods"];
-            modUpdate.NeedsUpdate = false;
-            modUpdate.Name = neosMod.Name;
-            modUpdate.OldVersion = neosMod.Version;
-            foreach (JToken mod in modList.Children())
-            {
-                if (mod.Path.Contains(neosMod.Name) & mod.ToString().Contains(neosMod.Author))
-                {
-                    int versions = 0;
-                    foreach (JToken version in mod.First["versions"])
+                    if (mod.Path.Contains(neosMod.Name) & mod.ToString().Contains(neosMod.Author))
                     {
-                        string newVersionStr = version.ToString().Split('"')[1];
-                        int newVersion = int.Parse(newVersionStr.Replace(".", ""));
-                        if (versions < newVersion)
+                        int versions = 0;
+                        foreach (JToken version in mod.First["versions"])
                         {
-                            versions = newVersion;
-                            modUpdate.NewVersion = newVersionStr;
-                            modUpdate.Url = (string)version.First["artifacts"][0]["url"];
-                            modUpdate.Sha256 = (string)version.First["artifacts"][0]["sha256"];
+                            string newVersionStr = version.ToString().Split('"')[1];
+                            int newVersion = int.Parse(newVersionStr.Replace(".", ""));
+                            if (versions < newVersion)
+                            {
+                                versions = newVersion;
+                                modUpdate.NewVersion = newVersionStr;
+                                modUpdate.Url = (string)version.First["artifacts"][0]["url"];
+                                modUpdate.Sha256 = (string)version.First["artifacts"][0]["sha256"];
+                            }
+                            else
+                            {
+                                break;
+                            }
                         }
-                        else
+
+                        // These are here due to the fact that some people are not consistant on how they use version numbers
+                        string oldVersion = neosMod.Version;
+                        if (neosMod.Version.Length > modUpdate.NewVersion.Length)
                         {
-                            break;
+                            oldVersion = neosMod.Version.Substring(0, modUpdate.NewVersion.Length);
+                        }
+
+                        if (modUpdate.NewVersion.Length > oldVersion.Length)
+                        {
+                            modUpdate.NewVersion = modUpdate.NewVersion.Substring(0, oldVersion.Length);
+                        }
+
+                        if (int.Parse(modUpdate.NewVersion.Replace(".", "")) > int.Parse(oldVersion.Replace(".", "")))
+                        {
+                            Warn(
+                                $"\n{modUpdate.Name}: New Version:{modUpdate.NewVersion} - Old Version: {modUpdate.OldVersion}\nUrl: {modUpdate.Url}\nSha256: {modUpdate.Sha256}");
                         }
                     }
-
-                    string oldVersion = neosMod.Version;
-                    if (neosMod.Version.Length > modUpdate.NewVersion.Length)
-                    {
-                        oldVersion = neosMod.Version.Substring(0, modUpdate.NewVersion.Length);
-                    }
-
-                    if (modUpdate.NewVersion.Length > oldVersion.Length)
-                    {
-                        modUpdate.NewVersion = modUpdate.NewVersion.Substring(0, oldVersion.Length);
-                    }
-
-                    if (int.Parse(modUpdate.NewVersion.Replace(".", "")) > int.Parse(oldVersion.Replace(".", "")))
-                    {
-                        modUpdate.NeedsUpdate = true;
-                    }
                 }
             }
-
-            return modUpdate;
         }
-
 
         private static string GetManifest(string url)
         {
@@ -186,39 +107,6 @@ namespace NMLUpdater
 
             string responseText = reader.ReadToEnd();
             return responseText;
-        }
-
-        private static void StartExternalProgram()
-        {
-            Msg("Starting External Mod Updater");
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                if (ModsThatNeedUpdating.Count > 0)
-                {
-                    if (_isSpawned == false)
-                    {
-                        var updaterArgs = " --neos-dir " + Directory.GetCurrentDirectory();
-                        var cmdArgs = "-u " + Directory.GetCurrentDirectory() + SpawnProgramDir + " " + updaterArgs;
-                        Warn(cmdArgs);
-                        _isSpawned = true;
-                        Process.Start(new ProcessStartInfo("bash", cmdArgs));
-                    }
-                }
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                if (ModsThatNeedUpdating.Count > 0)
-                {
-                    if (_isSpawned == false)
-                    {
-                        var updaterArgs = " --neos-dir " + Directory.GetCurrentDirectory();
-                        var cmdArgs = "/c " + Directory.GetCurrentDirectory() + SpawnProgramDir + " " + updaterArgs;
-                        Warn(cmdArgs);
-                        _isSpawned = true;
-                        Process.Start(new ProcessStartInfo("cmd", cmdArgs));
-                    }
-                }
-            }
         }
     }
 }
